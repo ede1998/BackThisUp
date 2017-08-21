19c36038e77a1fbd98492d0754f4a55cf2925c35
ede1998 <ede1998@arcor.de>
2017-08-21 17:34:42 +0200

    added threads

--- main.cpp
+++ main.cpp
@@ -1,27 +1,19 @@
 #include <iostream>
 #include <forward_list>
+#include <threads/mingw.thread.h>
 #include "Functions.h"
 #include "RootFolder.h"
 
 using std::string;
-/*int main() {
-    std::cout << "Hello, World!" << std::endl;
-    string path_absolute = "D:\\Users\\Erik\\CLionProjects\\googletest\\";
-    string path_relative = "..\\BackThisUp\\cmake-build-debug\\..";
-    string res = Backup::findAbsolutePath(path_absolute, path_relative);
-    std::cout << res << std::endl;
-    std::forward_list<string> files = Backup::findFiles(res);
-    std::forward_list<string> folders = Backup::findFolders(res);
-    for (string el: files)
-        std::cout << el << std::endl;
-    for (string el: folders)
-        std::cout << el << std::endl;
-    Backup::RootFolder rf("D:\\Users\\Erik", "");
-    return 0;
-}*/
 
-int main() {
+
+void execute() {
     Backup::RootFolder rf("D:\\Users\\Erik\\CLionProjects\\BackThisUp", "D:\\Users\\Erik\\Desktop\\Comp");
     rf.backup("D:\\Users\\Erik\\Desktop\\Test");
+}
+int main() {
+    using std::thread;
+    thread tes(execute);
+    tes.join();
     return 0;
 }
