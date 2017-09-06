5cd29ea3836a6b95fff18e926c07cb09a09aab70
ede1998 <ede1998@arcor.de>
2017-09-06 22:39:41 +0200

    updated readme

--- WebTools/pw_example.h
+++ WebTools/pw_example.h
@@ -4,5 +4,5 @@
 
 #pragma once
 
-constexpr char client_id[] = "6df12061-1ba8-4e21-b802-ddc10c63a63f";
-constexpr char client_secret[] = "wm8WiztuxtsZDjAHyxBhabT";
+constexpr char client_id[] = "123456a-bcde-333-b123-exa123mple";
+constexpr char client_secret[] = "ExampleSecret1235123";
